a41802533d3cb6ad5ddd4ae8ed3ba462c20c677f
bokenanuj <44374486+bokenanuj@users.noreply.github.com>
2020-09-05 18:57:20 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const url = 'https://github.com/bokenanuj/Pdfviewer/tree/master/docs';
+const url = 'https://github.com/bokenanuj/Pdfviewer/blob/master/docs/pdf.pdf';
 
 let pdfDoc = null,
   pageNum = 1,
